db9a6f007a57bbf872ddf8e25ab30dcb4e1ac617
yanagi0216 <0216yanagi@gmail.com>
2026-02-26 12:38:19 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,13 @@
+//変数を宣言
+let num = 11 ;
+
+//条件分岐
+if (num % 3 == 0 && num % 5 == 0){
+  console.log('3と5の倍数です');
+  }else if (num % 3 == 0) {
+    console.log('3の倍数です');
+  }else if (num % 5 == 0) {
+   console.log('5の倍数です');
+  }else {
+   console.log(num);
+}
