3bcd58266b2779db9dc9844c183e3be8665b1a1c
yanagi0216 <0216yanagi@gmail.com>
2026-03-03 10:13:14 +0900

    Add files via upload

---
+++ mdn.js
@@ -0,0 +1,9 @@
+const CurrentTime =new Date();
+
+const Year = CurrentTime.getFullYear();
+
+const CurrentMonth =CurrentTime.getMonth();
+
+const CurrentDate =CurrentTime.getDate();
+
+console.log(Year+"年"+CurrentMonth+"月"+CurrentDate+"日");
