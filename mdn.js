cb2478c7c15875b8951f5b712764da6542ba6510
yanagi0216 <0216yanagi@gmail.com>
2026-03-03 10:14:39 +0900

    Delete mdn.js

--- mdn.js
+++
@@ -1,9 +0,0 @@
-const CurrentTime =new Date();
-
-const Year = CurrentTime.getFullYear();
-
-const CurrentMonth =CurrentTime.getMonth();
-
-const CurrentDate =CurrentTime.getDate();
-
-console.log(Year+"年"+CurrentMonth+"月"+CurrentDate+"日");
